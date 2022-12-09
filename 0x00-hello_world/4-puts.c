72c235c5c93040e626416c4ef63922939aaf8bde
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-09 06:47:07 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-/*code to print string with puts*/
+/**
+ *  main- prints out programming is like building a multilingual puzzle
+ *  puts-command used to display
+ *
+ *  Return: 0 always success
+ */
 
 int main(void)
 {
 	char MyProg[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(MyProg);
 
 	return (0);
