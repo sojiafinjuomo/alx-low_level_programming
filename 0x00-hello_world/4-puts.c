13c70b7ff52c9fd1984507206d44c66ea4fcee9f
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-09 05:24:14 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /*code to print string with puts*/
 
 int main(void)
-{     
-	char MyProg[] = "\"Programming is like building a multilingual puzzle";	
+{
+	char MyProg[] = "\"Programming is like building a multilingual puzzle";
 	puts(MyProg);
+
 	return (0);
 }
 
