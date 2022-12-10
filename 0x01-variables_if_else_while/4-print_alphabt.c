b77c109396611de30da81f7145d0c0352ff8507a
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-10 14:17:43 -0800

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -18,18 +18,16 @@ int main(void)
 
 			while (ch <= 'z')
 
-				{
+			{
 
 				if (ch != 'e' && ch != 'q')
-
 					{
 
 						putchar(ch);
 
-					
 					}
 					ch++;
-				}
+			}
 
 				putchar('\n');
 
