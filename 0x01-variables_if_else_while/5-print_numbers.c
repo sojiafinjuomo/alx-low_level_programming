d0330f58be3045369a9b9c47427a6a9e2d6dbe53
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-11 14:52:18 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main- entry points
+ *
+ *Return: 0 always success
+ *
+ */
+
+int main(void)
+{
+	int a = 0;
+		while (a < 10)
+			printf("%d\n", a);
+		a++;
+
+		return (0);
+}
+
