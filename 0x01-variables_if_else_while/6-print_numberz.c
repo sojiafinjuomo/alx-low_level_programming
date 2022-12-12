a5379076365f153a318ec47459cc509f99e52614
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-11 17:42:34 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main- entry points
+ *
+ *Return: 0 always success
+ */
+
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+		putchar(num);
+		num++;
+	putchar('\n');
+
+	return (0);
+}
+
+
+
