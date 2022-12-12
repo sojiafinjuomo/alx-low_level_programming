0739c6db02e5ab7fc23fdc35f26a0a23c8504517
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-12 05:41:30 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main- entry poins
+ *
+ *Return: 0 always  Success
+ */
+int main(void)
+{
+
+	char cha;
+
+	for (cha = 'z'; cha >= 'a'; cha--)
+		putchar(cha);
+
+	putchar('\n');
+
+	return (0);
+
+
+
+
+}
