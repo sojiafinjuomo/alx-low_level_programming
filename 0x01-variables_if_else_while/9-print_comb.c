8e24ec30a5a7bc0cd8db7f1e9a2022493583e3e8
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-12 08:25:04 -0800

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,20 +13,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
-	
-	{
-		 putchar(n);
-		if (n != 57)
+		for (n = 48; n < 58; n++)
 		{
-			putchar(',');
-			putchar(' ');
-
+			putchar(n);
+			if (n != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 
-	}
+		putchar('\n');
 
-	putchar('\n');
-
-	return (0);
+		return (0);
 }
