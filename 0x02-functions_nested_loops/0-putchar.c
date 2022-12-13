068022b845a41cb909d9ef82d81568f9649e309b
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-13 11:53:51 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ *main- entry point for code to print putchar
+ *
+ * Return: 0 for success
+ *
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
+
