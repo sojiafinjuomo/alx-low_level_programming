faf259d9a537eef4ad7b04549f6e3d8a11b00465
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-14 03:14:06 -0800

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,14 +12,14 @@ void print_alphabet_x10(void)
 	char ch;
 	int i = 0;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
 	while (i < 11)
 	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
 		_putchar('\n');
 		i++;
-	}
-
 }
+
