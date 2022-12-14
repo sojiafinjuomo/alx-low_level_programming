db5d025d5f7ada9bdf32526eb474f29021fa2760
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-14 11:54:39 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * add- add two numbers
+ *
+ * @int: the parameter is an int value
+ *
+ * sum- holds the result of added numbers
+ *
+ * Return: Always Value
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
+
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
+/**
+ *
+ *
+ *
+ */
+
+int _islower(int c);
+{
+       if c islower
+	       return (0);
+       else
+	       return (1);
+       
+
+}
 
