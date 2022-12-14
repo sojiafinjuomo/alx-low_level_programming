e342d810b4dd5b3b70c5815a6b83815950437dda
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-14 05:26:27 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs- entry point
+ *
+ * Return: 0 always Success
+ *
+ * @x: gets absolute value
+ *
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+}
+
+
+
