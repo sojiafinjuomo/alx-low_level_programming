fa050f6200eaf39ba2b29b8acb88fbc5299d8595
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-14 02:21:38 -0800

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,25 @@
-#include <stdio.h
+#include "main.h"
+/**
+ * print_alphabet_x10- Entry points to print alphabet
+ *
+ * ten times
+ *
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
 
+	int i = 0;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		while (i < 11)
+		{
+			_putchar(ch);
+			_putchar('\n');
+			i++;
+		}
+	}
+}
