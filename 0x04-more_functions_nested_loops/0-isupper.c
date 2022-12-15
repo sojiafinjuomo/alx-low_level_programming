2a5ef3c05e0486335cdd56f6573763721cb6ae1a
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-15 13:11:31 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
+  * _isupper - entry points tocheck capital letter
   *
+  * @c: argument
   *
-  *
-  *
+  * Return: 1 and 0 Always success
   */
 
 int _isupper(int c)
 {
-if ( c is >= 'A' && c is <= 'Z')
+if (c is >= 'A' && c is <= 'Z')
 	return (1);
-else 
+else
 	return (0);
 }
-  
