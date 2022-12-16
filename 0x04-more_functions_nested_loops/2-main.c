3e492ed0535235ebf5eba4803658db7975bf7933
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-16 04:49:54 -0800

    mul

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -5,23 +5,13 @@
 
 
 /**
-
-   * main - check the code
-
-    *
-
-     * Return: Always 0.
-
-      */
-
+  * main - check the code
+  *
+  * Return: Always 0.
+  */
 int main(void)
-
 {
-
-	    printf("%d\n", mul(98, 1024));
-
-	        printf("%d\n", mul(-402, 4096));
-
-		    return (0);
-
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
 }
