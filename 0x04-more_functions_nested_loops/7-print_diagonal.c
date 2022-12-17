a82a65cb374dd4004b280c18af4a9aeb79b7d24e
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-17 08:14:00 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_diagonal - prints diagonal n times
+  *
+  * @n: number of times
+  *
+  * Return: No value
+  */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar("\");
+			_putchar('\n');
+		}
+}
+
