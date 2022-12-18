c488b12b73366493a581067d630f18cf5e175b7b
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-18 09:50:24 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,7 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n <= 0)
 	{
@@ -18,8 +19,12 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\"');
-		_putchar("\n");
+			_putchar('\n');
 		}
 	}
 }
