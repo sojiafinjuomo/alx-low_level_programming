dfb77b86e09880e21df1618c676daff97a451061
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-19 13:08:23 -0800

    Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * reset_to_98 - function to update value
+  *
+  * @n: pointer integer
+  *
+  * Return: No Return
+  *
+  */
+void reset_to_98(int *n);
+{
+	n = 98;
+	*p = n;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is Pointers, Arrays and Strings Project
