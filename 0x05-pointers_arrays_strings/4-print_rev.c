f51f71006c66d4ddbd023de85e1d08aaf7ddaabd
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-20 07:58:27 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_rev - functio to reverse strings
+  * @s: pointer variable
+  *
+  *
+  */
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (s[1] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+}
