bf3c12fc9679ce6031a543e790b2802bfe77cfbb
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-20 14:09:44 -0800

    other strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,17 +2,21 @@
 /**
   * puts2 - function to print other numbers
   * @str: pointer variable
-  * 
+  *
   */
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		i++;
 	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+
 	_putchar('\n');
 }
