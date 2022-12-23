79292a6dadb77788a8adfd5875a40d8ec1707f05
sojiafinjuomo <afinjuomosoji@gmail.com>
2022-12-23 14:36:33 -0800

    array revearse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * reverse_array - function to revearse content of arrays
+  * @a: array of integers to be revearsed
+  * @n: elements in the array
+  *
+  */
+void reverse_array(int *a, int n)
+{
+	int t, i;
+
+	for (i = n - 1; i >= n / 2; i++)
+	{
+		t = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = t;
+	}
+}
