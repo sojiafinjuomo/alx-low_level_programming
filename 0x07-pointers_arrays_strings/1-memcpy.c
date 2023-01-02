e2ce259eb237401ec57f7411cebff94d7f9344ae
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-02 13:53:39 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,11 @@
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	for (src[n] = 0; src[n] != '\0'; n++)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		dest = src++;
+		dest[i] = src[i];
 	}
 	return (dest);
 
