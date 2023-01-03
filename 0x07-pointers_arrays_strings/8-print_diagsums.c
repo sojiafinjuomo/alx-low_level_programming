90df74f7f5735cfd4384717272e136c5ebb34a9c
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-03 08:02:40 -0800

    diagonal sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_diagsums - adds sum of diagonal elements
+  * @a: matrix
+  * @size: size
+  * Return: return nothing
+  */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[(size + 1) * i];
+		sum2 += a[(size - 1) * (i + 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
