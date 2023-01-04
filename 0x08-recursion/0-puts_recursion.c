ba985df6f98a7dd8c95643edcc5617f00c363f0c
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-04 05:52:51 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
   * _puts_recursion - function that prints a string, followed by a new line
-  * @s hold string
+  * @s: pointing to char string
   *
   */
 void _puts_recursion(char *s)
 {
-	_putchar('s');
+	_putchar(*s);
 	_putchar('\n');
 }
