3558b5ed4018d7a7bbbbbcd03ffdc701181bd949
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-06 09:15:06 -0800

    lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,2 @@
-#include "main.h"
-/**
-  * _puts - function for puts
-  * @s: pointer parameter
-  */
-void _puts(char *s)
-{
-	while (*s != '\0')
-	{
-		_putchar(*s++);
-	}
-	_putchar('\n');
-}
+
+
