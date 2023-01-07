8bf1c10ed9fb453e86201392b4f51b6dd21e9965
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-07 13:54:38 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - entry point
+  * @argc: counter parameter
+  * @argv: argument vector
+  * Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
