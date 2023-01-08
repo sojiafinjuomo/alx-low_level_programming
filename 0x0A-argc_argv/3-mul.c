5aeae1ab696cabf4df55ba3a09f099f0c83d3b6e
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-08 08:49:21 -0800

    multiply vectors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,18 @@ int main(int argc, char *argv[])
 	int mul = 1;
 	int i;
 
-	if (argc <= 1)
+	if (argc == 3)
 	{
-		printf("Error\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		for (i = 1; i < 3; i++)
 		{
 			mul = mul * atoi(argv[i]);
 		}
 		printf("%d\n", mul);
 		return (0);
 	}
-	return (1);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
