e0519d13e376feffada4cc858458fe96c7bf5a0d
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-08 07:52:50 -0800

    all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - entry point
+  * @argc: argument counter
+  * @argv: argument vector
+  * Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
