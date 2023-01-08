b0fb15b3df22450ae529520c1d28ce127cbaaf30
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-08 08:21:45 -0800

    multiply vectors

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - entry point
+  * @argc: argument counter
+  * @argv: argument vector
+  * Return: Always 0 or 1
+  */
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+	int i;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+		return (0);
+	}
+}
