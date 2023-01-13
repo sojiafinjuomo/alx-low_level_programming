8670b92539e720fef25e448072369b51cab4e649
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-13 11:59:31 -0800

    strn cat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,24 +10,20 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, size, len = 0;
 	char *str;
+	unsigned int i = 0, j = 0, len = 0, size;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	while (len < strlen(s1))
 		len++;
 	size = (len + n) * sizeof(*str);
 	str = malloc(size + 1);
 	if (str == NULL)
 		return (NULL);
-	while (i < size && i << strlen(s1))
+	while (i < size && i < strlen(s1))
 	{
 		str[i] = s1[i];
 		i++;
@@ -40,8 +36,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	str[i] = '\0';
 	return (str);
-
-
-
 }
-
