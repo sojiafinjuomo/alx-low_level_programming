0fdd47076d5029012e22c8e7ab88084d1d4747b2
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-13 14:25:19 -0800

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	unsigned *ptr = malloc(sizeof(nmemb) * size);
+	unsigned int *ptr = malloc(nmemb * size);
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
