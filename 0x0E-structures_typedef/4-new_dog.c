06f3c9a4c7cdccb93d36103aa362bcc01b799b82
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-16 15:13:16 -0800

    free

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+  * new_dog - function that creates a new dog
+  * @name: argument1
+  * @age: float argument
+  * @owner: argument
+  * Return: Return Null if function fails
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	if (new_dog == NULL)
+		return (NULL);
+	*(new) = d.name;
+	*(new) = d.owner;
+	return (new);
+
+
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+  * free_dog - function free memory storage containing dogs
+  * @d: struct variable
+  * Return: 0 or Null
+  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+	}
+}
