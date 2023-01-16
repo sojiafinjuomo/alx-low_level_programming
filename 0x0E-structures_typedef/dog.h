1ca78d63631797bbd0db9fdbd651fb9202abb96e
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-16 10:56:05 -0800

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - defines a new type
+  * @name: member1
+  * @age: member2
+  * @owner: member3
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
