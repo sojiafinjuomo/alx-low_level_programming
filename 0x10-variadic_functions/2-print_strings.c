836aa4a30cdce26917e0434d15757321b495f2e3
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-19 12:31:55 -0800

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			ch = va_arg(args, char *);
-			if (ch[i] == '\0')
+			if (ch == NULL)
 				printf("%s", "(nil)");
 			else
 				printf("%s", ch);
