07774b8049a9cfb3d6d8845753b79be6775f4125
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-19 09:15:04 -0800

    sum of arg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,17 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i =1;
+	int count, total = 0;
+
 	if (n == 0)
 		return (0);
-	va_list args;
-	unsigned int i = 1;
-
 	va_start(args, n);
-	unsigned int count = va_arg(args, unsigned int);
-	unsigned int total = count;
-
 	while (i < n)
 	{
-		count = va_arg(args, unsigned int);
-		total += count;
+		count = va_arg(args, int);
+		total = total + count;
 		i++;
 	}
 	va_end(args);
