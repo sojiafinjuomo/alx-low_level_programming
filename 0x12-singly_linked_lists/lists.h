49a7932d74abc55112e75c1014a8ae46b7cd382b
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-01-26 15:46:14 -0800

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,15 +2,24 @@
 #define LIST_H
 #include <stddef.h>
 
-typedef struct node{
+/**
+  * struct node - defined datatype
+  * @str: string;
+  * @len: element of struct
+  * @next: link /ptr to next node
+  */
+typedef struct node
+{
 	char *str;
 	unsigned int len;
 	struct node *next;
-}list_t;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 
