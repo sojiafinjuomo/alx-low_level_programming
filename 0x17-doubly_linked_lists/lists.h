a6d8d7f0a54e0863306b50d8435d45b75db49725
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-03-17 15:39:53 -0700

    print list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -10,11 +10,11 @@
   * Description: doubly linked list node structure
   *
   */
-typedef struct dlistint_t
+typedef struct dlistint_s
 {
 	int n;
-	struct dlistint_t *prev;
-	struct dlistint_t *next;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
