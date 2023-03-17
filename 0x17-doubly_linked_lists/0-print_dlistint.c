760c562c6b98b6f715e3a005c59fc1b2476bac53
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-03-17 13:02:30 -0700

    print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+  * print_dlistint - function that prints all the elements of a dlistint_t list
+  * @h: points to the struct node
+  * Return: return node count
+  *
+  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int node_count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d elements\n", h->n);
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,9 @@
+0x17. C - Doubly linked lists
+C
+Algorithm
+Data structure
+ By: Julien Barbier
+ Weight: 1
+ Project will start Mar 16, 2023 6:00 AM, must end by Mar 17, 2023 6:00 AM
+ Checker was released at Mar 16, 2023 12:00 PM
+ An auto review will be launched at the deadline
