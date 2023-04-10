aff28da69f67c61028ba48d0dd1e3c4c0f257184
sojiafinjuomo <afinjuomosoji@gmail.com>
2023-04-10 12:08:17 -0700

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,4 @@
-3include "hash_tables.h"
+#include "hash_tables.h"
 
 /**
   * hash_djb2 - hash fn for djb2 algorithm
@@ -10,7 +10,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash;
 	int k;
 
-	hash = 5381
+	hash = 5381;
 		while ((k = *str++))
 			hash = ((hash << 5) + hash) + k;
 	return (hash);
